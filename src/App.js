a47631b241682b6451ce0b57e3325e918cc5a0eb
anca0 <anca.rada24@gmail.com>
2022-09-06 08:12:03 +0300

    display employees names in grid cells - content

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Employees from "./Employees";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
